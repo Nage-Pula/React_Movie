83e49c6a83e14e0e99912d40fec0cf6272ac95d0
Nage-Pula <pulanageswararao1@gmail.com>
2025-07-30 14:40:37 +0530

    modified package.json and vite.config.js and indexhtml modified the title name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// ✅ Add the 'base' config below
+export default defineConfig({
+  base: '/React_Movie/',
+  plugins: [react()],
 })
+
